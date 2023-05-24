a39f70075eccd536e1d651a637e027bb3428baa7
byzze <wxluoyouda@gfpartner.com.cn>
2023-05-24 13:56:40 +0800

    upd sh

--- relese.sh
+++ relese.sh
@@ -3,7 +3,7 @@
 # 使用hugo编译生成public文件夹, 提供public文件内容即可部署博客内容, 本次使用github提供的byzze.github.io部署博客, 需要将public的文件内容上传到指定仓库git@github.com:byzze/byzze.github.io.git, 即可访问https://byzze.github.io/
 
 # 迁移文件夹
-cp -r public ../ && cd ../public
+cd public
 
 # 填写 Git 仓库信息
 GIT_REPO="git@github.com:byzze/byzze.github.io.git"
@@ -12,7 +12,6 @@ GIT_USERNAME="byzze"
 
 # # 提交信息
 COMMIT_MESSAGE="自动提交于 $(date)"
-git init
 # # 添加所有更改文件到 git
 git add .
 
@@ -27,4 +26,3 @@ git push $GIT_REPO $GIT_BRANCH -f
 # git config user.email $GIT_EMAIL
 
 cd -
-rm -rf public
