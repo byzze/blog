090b80c9f437039b764e28a2654ae4d3e18e3dd9
byzze <wxluoyouda@gfpartner.com.cn>
2024-02-26 10:05:05 +0800

    自动提交于 2024年 02月 26日 星期一 10:05:01 CST

--- relese.sh
+++ relese.sh
@@ -3,7 +3,7 @@
 # 使用hugo编译生成public文件夹, 提供public文件内容即可部署博客内容, 本次使用github提供的byzze.github.io部署博客, 需要将public的文件内容上传到指定仓库git@github.com:byzze/byzze.github.io.git, 即可访问https://byzze.github.io/
 
 # 将编译后的博客内容发布byzze.github.io
-编译hugo 
+echo 编译hugo 
 hugo
 # 迁移文件夹 git commit pushlic
 cd public
